8dcd66be0eda791365beda082a952abf652da97b
saltzmanjoelh <talk@joelsaltzman.com>
2018-08-30 23:27:49 -0700

    Fixed typo

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "http://github.com/saltzmajoelh/NSTokenAttachmentCell", .branch("master")),
+        .package(url: "http://github.com/saltzmanjoelh/NSTokenAttachmentCell", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
